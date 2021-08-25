95f65160260e767961035d5807955e62ea611734
rgilles <rgilles@student.42.fr>
2021-08-25 13:54:26 +0200

    draft for ccon to exec

--- parser/ccon_to_exec.c
+++ parser/ccon_to_exec.c
@@ -31,12 +31,51 @@ char	**strtabdup(char **tab, int is_alone)
 	return (ret);
 }
 
-int	ccon_to_exec(struct s_cmdcontent *ccon)
+int	ccon_to_exec(t_cmdcontent *ccon_full, t_minishell *m)
 {
-	char	**cmd;
+	char			**content;
+	t_ioitem		*curr;
+	t_exectype		binbuiltin;
+	t_cmdcontent	*ccon;
 
-	cmd = strtabdup(ccon->cmd, 0);
-	if (!cmd)
-		return (-1);
+	ccon = ccon_full;
+	while (ccon)
+	{
+		curr = ccon->inputs
+		while (curr)
+		{
+			content = strtabdup(&curr->name, 1);
+			if (!content)
+				return (-1);
+			if (curr->type == IN)
+				exec_builder(&m->ed, content, INPUT, 1);
+			else
+				exec_builder(&m->ed, content, DINPUT, 1);
+			curr = curr->next
+		}
+		content = strtabdup(ccon->cmd, 0);
+		if (!content)
+			return (-1);
+		binbuiltin = BINARY;
+		if (is_builtin(content))
+			binbuiltin = BUILTIN;
+		if (!ccon->outputs && ccon->sep_type == ';')
+			exec_builder(&m->ed, content, binbuiltin, 0);
+		else
+			exec_builder(&m->ed, content, binbuiltin, 1);
+		curr = ccon->outputs;
+		while (curr)
+		{
+			content = strtabdup(&curr->name, 1);
+			if (!content)
+				return (-1);
+			if (curr->type == OUT)
+				exec_builder(&m->ed, content, OUTPUT, 1);
+			else
+				exec_builder(&m->ed, content, DOUTPUT, 1);
+			curr = curr->next
+		}
+		ccon = ccon->next;
+	}
 	return (0);
 }
