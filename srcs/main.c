3d75c75cea0d6b2acb6bf27da2f62df4dc3a9485
Nayel OUCHATAR <nouchata@e2r9p17.42.fr>
2021-08-25 15:53:05 +0200

    edited main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nouchata <nouchata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 10:52:45 by marvin            #+#    #+#             */
-/*   Updated: 2021/08/25 15:19:35 by nouchata         ###   ########.fr       */
+/*   Updated: 2021/08/25 15:50:15 by nouchata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void sig_reset_prompt(int signal)
 //	return (0);
 //}
 
-/*int	mainb(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **env)
 {
 	t_minishell		m;
 	char			*line;
@@ -47,39 +47,40 @@ void sig_reset_prompt(int signal)
 		line = readline("pasdebashing$ ");
 		if (line && line[0]) // && !exec_maker(&m, line))
 		{
-			// add_history(line);
-			// m.ve.bin_return = exec_loop(m.ed, &m.ve);
-			// exec_killer(m.ed);
+			add_history(line);
+			parsing_tests(&m, line);
+			m.ve.bin_return = exec_loop(m.ed, &m.ve);
+			exec_killer(m.ed);
 			m.ed = NULL;
 		}
 	}
 	builtin_exit(m.ve.minishell_var, NULL);
 	return (0);
-}*/
-
-int	main(int argc, char **argv, char **env)
-{
-	t_minishell		m;
-	char			*line;
-
-	(void)argv;
-	(void)argc;
-	/* BINARY & BUILTIN */
-	// char	*bin[] = {"cmd", "arg", NULL};
-	// char *cat[] = {"cat", NULL};
-	// /* (D)INPUT & (D)OUTPUT */
-	// char	*file[] = {"pathtofile", NULL};
-
-	m.ed = NULL;
-	m.ve = varenv_construct(&m, env);
-	m.ve.env_to_str = env_to_str(&m.ve);
-	line = "ls";
-	parsing_tests(&m, line);
-	m.ve.bin_return = exec_loop(m.ed, &m.ve);
-
-	builtin_exit(&m, NULL);
-	return (0);
 }
+
+// int	main(int argc, char **argv, char **env)
+// {
+// 	t_minishell		m;
+// 	char			*line;
+
+// 	(void)argv;
+// 	(void)argc;
+// 	/* BINARY & BUILTIN */
+// 	// char	*bin[] = {"cmd", "arg", NULL};
+// 	// char *cat[] = {"cat", NULL};
+// 	// /* (D)INPUT & (D)OUTPUT */
+// 	// char	*file[] = {"pathtofile", NULL};
+
+// 	m.ed = NULL;
+// 	m.ve = varenv_construct(&m, env);
+// 	m.ve.env_to_str = env_to_str(&m.ve);
+// 	line = "wjdfhvghu";
+// 	parsing_tests(&m, line);
+// 	m.ve.bin_return = exec_loop(m.ed, &m.ve);
+
+// 	builtin_exit(&m, NULL);
+// 	return (0);
+// }
 
 int parsing_tests(t_minishell *m, char *line)
 {
@@ -92,7 +93,7 @@ int parsing_tests(t_minishell *m, char *line)
 	t_ioitem		*tmp3 = NULL;
 	t_cmdcontent	*tmp4 = NULL;
 
-	printf("[%s]\n%d\n", line, count_all_segments(line));
+	// printf("[%s]\n%d\n", line, count_all_segments(line));
 	cmdc = chunk_list_creator(line);
 	if (!cmdc)
 		return (0);
@@ -114,44 +115,44 @@ int parsing_tests(t_minishell *m, char *line)
 	ccon = NULL;
 	while (tmp2)
 	{
-		i = 0;
-		while (tmp2->segments && tmp2->segments[i])
-		{
-			printf("[%s]\n", tmp2->segments[i]);
-			i++;
-		}
+		// i = 0;
+		// while (tmp2->segments && tmp2->segments[i])
+		// {
+		// 	printf("[%s]\n", tmp2->segments[i]);
+		// 	i++;
+		// }
 		cmdcontent_builder(&ccon, tmp2);
 		tmp2 = tmp2->next;
 	}
-	printf("\n");
-	tmp4 = ccon;
-	while (tmp4)
-	{
-		printf("current cmdcontent:\n");
-		tmp3 = tmp4->inputs;
-		if (tmp3)
-			printf("inputs:\n");
-		while (tmp3)
-		{
-			printf("{[%d]::%s}\n", tmp3->type, tmp3->name);
-			tmp3 = tmp3->next;
-		}
-		tmp3 = tmp4->outputs;
-		if (tmp3)
-			printf("outputs:\n");
-		while (tmp3)
-		{
-			printf("{[%d]::%s}\n", tmp3->type, tmp3->name);
-			tmp3 = tmp3->next;
-		}
-		i = 0;
-		while (tmp4->cmd && tmp4->cmd[i])
-		{
-			printf("cmd[%s]\n", tmp4->cmd[i]);
-			i++;
-		}
-		tmp4 = tmp4->next;
-	}
+	// printf("\n");
+	// tmp4 = ccon;
+	// while (tmp4)
+	// {
+	// 	printf("current cmdcontent:\n");
+	// 	tmp3 = tmp4->inputs;
+	// 	if (tmp3)
+	// 		printf("inputs:\n");
+	// 	while (tmp3)
+	// 	{
+	// 		printf("{[%d]::%s}\n", tmp3->type, tmp3->name);
+	// 		tmp3 = tmp3->next;
+	// 	}
+	// 	tmp3 = tmp4->outputs;
+	// 	if (tmp3)
+	// 		printf("outputs:\n");
+	// 	while (tmp3)
+	// 	{
+	// 		printf("{[%d]::%s}\n", tmp3->type, tmp3->name);
+	// 		tmp3 = tmp3->next;
+	// 	}
+	// 	i = 0;
+	// 	while (tmp4->cmd && tmp4->cmd[i])
+	// 	{
+	// 		printf("cmd[%s]\n", tmp4->cmd[i]);
+	// 		i++;
+	// 	}
+	// 	tmp4 = tmp4->next;
+	// }
 	chunkseg_killer(cs);
 	ccon_to_exec(ccon, m);
 	cmdcontent_killer(ccon);
